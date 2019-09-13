515f3225b9c6647d2e114d2be6e6b8d9c23e5e58
Rashi Bansal <rashibansal79518@gmail.com>
2019-09-13 17:39:19 +0530

    Get Change
    Get change
    You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
    
    Item	Cost
    Candy	$1.42
    Chips	$2.40
    Soda	$1.00
    Create 5 variables and use the cost from the table above to set their values.
    
    money
    candy
    chips
    soda

---
+++ Grasshopper_make_change.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 17:36:52 2019
+
+Get change
+You go to the store and have a 10 dollar bill to spend. You buy candy, chips, and soda. Find out how much change you get back from the cashier.
+
+Item	Cost
+Candy	$1.42
+Chips	$2.40
+Soda	$1.00
+Create 5 variables and use the cost from the table above to set their values.
+
+money
+candy
+chips
+soda
+"""
+
+#your code here
+money=10 #Total money we have is 10 dollar
+candy=1.42 #cost of candy
+chips=2.40 #cost of chips
+soda=1.00 #cost of soda
+change=money-(candy+chips+soda) #remaining money
+print(change) 
+
+
+
+
+
+#@author rashi
