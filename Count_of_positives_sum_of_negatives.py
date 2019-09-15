10b4a050923471048f125192d62ef8ae12a3956d
Rashi Bansal <rashibansal79518@gmail.com>
2019-09-15 12:28:03 +0530

    Count_of_positives/sum_of_negatives
    Description:
    Given an array of integers.
    
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
    
    If the input array is empty or null, return an empty array.
    
    Example
    For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].

---
+++ Count_of_positives_sum_of_negatives.py
@@ -0,0 +1,37 @@
+"""Description:
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+If the input array is empty or null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+"""
+
+
+##Clever approach
+
+
+def count_positives_sum_negatives(arr):
+    return [len([x for x in arr if x > 0])] + [sum(y for y in arr if y < 0)] if arr else []
+
+
+
+
+"""
+
+
+##My Approach
+    
+def count_positives_sum_negatives(arr):
+    if not arr: return []
+    pos = 0
+    neg = 0
+    for x in arr:
+      if x > 0:
+          pos += 1
+      if x < 0:
+          neg += x
+    return [pos, neg]
+"""
