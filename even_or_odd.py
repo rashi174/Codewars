1ad1942468662325815fcae7c72b470d82b4cc59
Rashi Bansal <rashibansal79518@gmail.com>
2019-09-17 20:02:40 +0530

    Even or odd
    Description:
    Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.

---
+++ even_or_odd.py
@@ -0,0 +1,18 @@
+
+##Description:
+"""Create a function (or write a script in Shell) that takes an integer as 
+an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+"""
+"""
+
+def even_or_odd(number):
+  return 'Odd' if number % 2 else 'Even'
+  
+"""
+
+
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
