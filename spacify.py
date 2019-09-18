2ceb7a34b39e1bfcbf205395c12941bd7b728bf4
Rashi Bansal <rashibansal79518@gmail.com>
2019-09-18 18:13:05 +0530

    Spacify
    Description:
    Modify the spacify function so that it returns the given string with spaces insertedbetween each character.
    
    spacify("hello world") # returns "h e l l o   w o r l d"

---
+++ spacify.py
@@ -0,0 +1,13 @@
+"""
+Description:
+Modify the spacify function so that it returns the given string with spaces insertedbetween each character.
+
+spacify("hello world") # returns "h e l l o   w o r l d"
+
+
+
+"""
+
+
+def spacify(string):
+    return " ".join(string)
